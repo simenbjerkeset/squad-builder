1c5043734fe47b60c7c6b670b136412e8045bdbe
Simen Bjerkeset Andersen <simen.andersen@tripletex.no>
2020-04-23 12:28:31 +0200

    Added more arenas

--- src/data/arenas/arenas.js
+++ src/data/arenas/arenas.js
@@ -2,6 +2,22 @@ const ARENAS = [
     {
         name: "Gamle Briskeby stadion",
         value: 92
+    },
+    {
+        name: "Nye Briskeby stadion",
+        value: 87
+    },
+    {
+        name: "HIAS",
+        value: 50
+    },
+    {
+        name: "Børstad",
+        value: 40
+    },
+    {
+        name: "Prestrud",
+        value: 45
     }
 ]
 
