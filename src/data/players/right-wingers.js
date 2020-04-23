3f151634ff96abac45d8a7d9ad419ca128101ff4
BAnderseen <63196686+BAnderseen@users.noreply.github.com>
2020-04-23 12:28:46 +0200

    Update right-wingers.js

--- src/data/players/right-wingers.js
+++ src/data/players/right-wingers.js
@@ -4,6 +4,30 @@ const RIGHT_WINGERS = [
         name: "Ahmed Daghim",
         position: "RW",
         score: 71
+    },
+    {
+        id: 9002,
+        name: "Marius Gullerud",
+        position: "RW",
+        score: 75
+    },
+    {
+        id: 9003,
+        name: "Pål Alexander Kirkevold",
+        position: "RW",
+        score: 79
+    },
+    {
+        id: 9004,
+        name: "Adem Güven",
+        position: "RW",
+        score: 65
+    },
+    {
+        id: 9005,
+        name: "Stian Berget",
+        position: "RW",
+        score: 78
     }
 ];
 
